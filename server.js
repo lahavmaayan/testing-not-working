7d6eaeb9a25e162194c5ccc9f4f53ecfb9864261
Maayan <lahav.maayan@gmail.com>
2016-07-26 15:12:24 +0300

    server + added frontend

--- server.js
+++ server.js
@@ -3,3 +3,28 @@
 // will hold the connection to the mogoDB.
 var mongo_driver = require('./mongos_driver.js');
 mongo_driver.initConnection();
+var simpleToDo = mongo_driver.initSchema({
+  TodoName: String,
+  assignTo: String,
+  Date: String,
+  whoAdded: String
+});
+
+var simpleTodoModel = mongo_driver.getModel('ToDos', simpleToDo);
+console.log(simpleTodoModel);
+
+var express = require('express');
+var app = express();
+
+app.get('*', function(){
+    console.log('got request GET');
+});
+
+app.post('*', function(){
+    console.log('got request POST');
+})
+
+
+app.listen(3000, function(){
+    console.log('app is listening on port 3000');
+});
